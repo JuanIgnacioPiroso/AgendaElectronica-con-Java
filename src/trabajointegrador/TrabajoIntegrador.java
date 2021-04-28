96a697db5755f329cac0df379a7157ee5a987c67
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2021-04-28 01:20:11 -0300

    JAVA

---
+++ src/trabajointegrador/TrabajoIntegrador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajointegrador;
+import IGU.interfaz;
+
+public class TrabajoIntegrador {
+
+    public static void main(String[] args) {
+        
+        interfaz main = new interfaz();
+        main.setLocationRelativeTo(null);
+        main.setVisible(true);
+        
+    }
+    
+}
